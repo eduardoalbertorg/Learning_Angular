4089ed323b5a01dd8b0adae61d3e207d0cf1adfa
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2020-07-13 09:44:51 -0500

    Get recipes using ngfor directive

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Enchiladas de pollo', 'Son enchiladas y tienen pollo', 'https://images-gmi-pmc.edge-generalmills.com/0f58d310-021e-4175-b1b6-d73be26bac7e.jpg'),
+    new Recipe('Pollo Qinn', 'Es adictivo', 'https://www.qin.mx/img/img_carrusel_02.png')
+  ];
 
   constructor() { }
 
